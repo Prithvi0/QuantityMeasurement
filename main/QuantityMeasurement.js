3529c7474ae626e63d4d711513b8b898783d9fd8
Prithvi0 <prithviy96@gmail.com>
2020-04-24 07:02:06 +0530

    Modified the code by changing the naming conventions

--- main/QuantityMeasurement.js
+++ main/QuantityMeasurement.js
@@ -3,14 +3,15 @@ var QuantityMeasurement = function(length) {
     this.length = length;
   };
   
-// Method to get inch to feet conversion
-QuantityMeasurement.prototype.getInchToFeetConversion = function() {
+// Method to get feet to inch conversion
+QuantityMeasurement.prototype.getFeetToInchConversion = function() {
     return this.length * 12;
 };
 
-// Method to get feet to inch conversion
-QuantityMeasurement.prototype.getFeetToInchConversion = function() {
+// Method to get inch to feet conversion
+QuantityMeasurement.prototype.getInchToFeetConversion = function() {
     return this.length / 12;
 }
-  
+
+// Method to get 
 module.exports = QuantityMeasurement;
